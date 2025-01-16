1a2f49617e7da251a3cc670965c81067d1be1965
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-16 13:21:24 -0300

    carousel circle v4.8.4

--- imperiosur.php
+++ imperiosur.php
@@ -28,6 +28,6 @@ function create_block_imperiosur_block_init() {
 	register_block_type( __DIR__ . '/build/imperiosur' );
 
 	    // Registrar el nuevo bloque
-		register_block_type( __DIR__ . '/build/Bloque-nuevo');
+		register_block_type( __DIR__ . '/build/Bloque-nuevo' );
 }
 add_action( 'init', 'create_block_imperiosur_block_init' );
