12b8a0323770429369a87b6768d65984c6fabcad
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-16 12:02:08 -0300

    tests

--- build/Bloque-nuevo/render.php
+++ build/Bloque-nuevo/render.php
@@ -1,40 +1,5 @@
 
 
-<div class="circle-container">
-  <div class="circle">
-    <a href="#">
-      <img src="https://bellarosatta.com/wp-content/uploads/2025/01/D_Q_NP_881108-MLA81635198923_122024-F00.jpg-G.webp" alt="Mujer">
-      
-    </a>
-  </div>
-  <div class="circle">
-    <a href="#">
-      <img src="https://bellarosatta.com/wp-content/uploads/2025/01/D_Q_NP_827836-MLA81635255805_122024-G.webp" alt="Hombre">
-      
-    </a>
-  </div>
-  <div class="circle">
-    <a href="#">
-      <img src="https://bellarosatta.com/wp-content/uploads/2025/01/D_Q_NP_707103-MLA78525377724_082024-F00.jpg-G.webp" alt="Niños">
-      
-    </a>
-  </div>
-  <div class="circle">
-    <a href="#">
-      <img src="https://bellarosatta.com/wp-content/uploads/2025/01/D_Q_NP_785900-MLA78758415905_082024-F00.jpg-G.webp" alt="Bebés">
-      
-    </a>
-  </div>
-  <div class="circle">
-    <a href="#">
-      <img src="https://bellarosatta.com/wp-content/uploads/2025/01/D_Q_NP_723219-MLA78525172898_082024-F00.jpg-G.webp" alt="Ofertas del día">
-      
-    </a>
-  </div>
-  <div class="circle">
-    <a href="#">
-      <img src="https://bellarosatta.com/wp-content/uploads/2025/01/D_Q_NP_875227-MLA79883989856_102024-G.webp" alt="Fábrica de tendencias">
-      
-    </a>
-  </div>
+<div class="hola">
+    <p>hola mundo!</p>
 </div>
