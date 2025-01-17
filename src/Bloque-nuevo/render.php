fcc4a79132ea29baf275339d7cdcb47833bcb1f6
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-17 00:05:49 -0300

    carousel circle semiFin2

--- src/Bloque-nuevo/render.php
+++ src/Bloque-nuevo/render.php
@@ -13,7 +13,7 @@
 <?php
 
 echo "<script>console.log('Mensaje desde render.php');</script>";
-echo "<script>console.log('Mensaje: " . $attributes . "');</script>";
+echo "<script>console.log('Mensaje: " . $attributes[0] . "');</script>";
 function render_bloque_dinamico( $attributes, $content ) {
     // Verifica si el atributo 'images' existe y no está vacío
     echo 'la funcion se ejecuto.' . PHP_EOL;
