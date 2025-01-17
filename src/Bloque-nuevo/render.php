fea1ac72c756e425e236ea24a741b37158c35a2c
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-17 01:30:27 -0300

    carousel circle Fin6

--- src/Bloque-nuevo/render.php
+++ src/Bloque-nuevo/render.php
@@ -5,15 +5,15 @@
 <?php
 
 // Supongamos que $attributes contiene el JSON que mostraste
-//$images = $attributes['images']; // Accedemos al array de imágenes
+$images = $attributes['images']; // Accedemos al array de imágenes
 
 // Inicializamos variables para guardar las URLs
-//$url1 = isset($attributes['images'][i]['url']) ? $attributes['images'][i]['url'] : null;
-//$alt1 = isset($attributes['images'][i]['alt']) ? $attributes['images'][i]['alt'] : null;
-//$length = count($images);
+$url1 = isset($images[0]['url']) ? $images[0]['url'] : null;
+$alt1 = isset($attributes['images'][0]['alt']) ? $attributes['images'][0]['alt'] : null;
+$length = count($images);
 
-echo "<script>console.log('Mensaje: " . ($attributes['images'][0]['url'] ? $attributes['images'][0]['url']) : null . "');</script>";
-echo "<script>console.log('Mensaje: " . ($attributes['images'][0]['alt'] ? $attributes['images'][0]['alt']) : null . "');</script>";
+echo "<script>console.log('Mensaje: " . $url . "');</script>";
+echo "<script>console.log('Mensaje: " . ($attributes['images'][0]['alt'] ? $attributes['images'][0]['alt'] : null) . "');</script>";
 echo "<script>console.log('Mensaje: " . count($attributes['images']) . "');</script>";
 //echo "<script>console.log('Mensaje:', " . json_encode($attributes) . ");</script>";
 
