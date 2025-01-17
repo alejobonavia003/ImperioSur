64a4bdaeab37aa4939c2e2b2544bb96caf007446
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-16 23:55:40 -0300

    carousel circle supertes208

--- src/Bloque-nuevo/render.php
+++ src/Bloque-nuevo/render.php
@@ -11,8 +11,9 @@
 </div>
 
 <?php
+
 echo "<script>console.log('Mensaje desde render.php');</script>";
-echo "<script>console.log('Mensaje: " . $args . "');</script>";
+echo "<script>console.log('Mensaje: " . $args['images'] . "');</script>";
 function render_bloque_dinamico( $attributes, $content ) {
     // Verifica si el atributo 'images' existe y no está vacío
     echo 'la funcion se ejecuto.' . PHP_EOL;
