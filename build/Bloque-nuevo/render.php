a5a58cd2c068d0b30f71dbce3596e477ae2facaa
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-25 13:26:37 -0300

    casilla de links a bloques de circulos v1.1.6

--- build/Bloque-nuevo/render.php
+++ build/Bloque-nuevo/render.php
@@ -24,7 +24,8 @@ $length = count($images);
 
 echo "<script>console.log('Mensaje: " . ($attributes['images'][0]['url'] ? $attributes['images'][0]['url'] : null) . "');</script>";
 echo "<script>console.log('Mensaje: " . ($attributes['images'][0]['alt'] ? $attributes['images'][0]['alt'] : null) . "');</script>";
-echo "<script>console.log('Mensaje: " . json_encode($attributes['images'][0]) . "');</script>";
+echo "<script>console.log('Mensaje: " . ($attributes['images'][0]['link'] ? $attributes['images'][0]['link'] : null) . "');</script>";
+echo "<script>console.log('Mensaje: " . json_encode($attributes['images'][0]['link']) . "');</script>";
 echo "<script>console.log('Mensaje: " . count($attributes['images']) . "');</script>";
 //echo "<script>console.log('Mensaje:', " . json_encode($attributes) . ");</script>";
 
