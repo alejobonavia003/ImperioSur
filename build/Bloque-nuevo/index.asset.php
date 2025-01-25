07f1271aab10ba94627ed0d1598d97f2fcf0bb75
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-25 14:48:54 -0300

    ultimos cambios v1

--- build/Bloque-nuevo/index.asset.php
+++ build/Bloque-nuevo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'fadce12257c87acea546');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c6393ef8e79ba564c093');
