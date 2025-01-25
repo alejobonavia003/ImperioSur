0efe227fc95d75cc02fd30d96d6b96952b2e3f87
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-25 14:41:33 -0300

    estilos chek

--- build/Bloque-nuevo/index.asset.php
+++ build/Bloque-nuevo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3f36abc9996891576f0f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'fadce12257c87acea546');
