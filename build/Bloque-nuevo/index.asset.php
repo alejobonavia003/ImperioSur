fc63e1228c29c28eb16261483a67bc7137530821
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-25 12:58:06 -0300

    casilla de links a bloques de circulos v1.1.2

--- build/Bloque-nuevo/index.asset.php
+++ build/Bloque-nuevo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c5492830916a64dc10b8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '01e213dc328f2da22ebc');
