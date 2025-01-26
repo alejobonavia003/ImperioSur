472790909e6bdcc6a6c6255e98738053daa8971f
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-26 19:20:59 -0300

    retoques v3

--- build/Bloque-nuevo/index.asset.php
+++ build/Bloque-nuevo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '66f7182f1e88404faff4');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2fefecba8b8e78bb9476');
