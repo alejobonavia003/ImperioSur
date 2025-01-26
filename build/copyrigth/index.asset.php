88b49962a66c115f7e18b960154572b886fd2471
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-26 16:40:58 -0300

    practicando creacion de bloques v1.0.3

--- build/copyrigth/index.asset.php
+++ build/copyrigth/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '49c2bff5c899db0a225a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '2369a3857c9018e2af5f');
