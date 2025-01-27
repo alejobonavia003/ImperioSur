b48ed9bac50be8d383207fefafa769981914c2f2
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-26 21:32:43 -0300

    bloque de ofertas v1.0.4

--- src/ofertas/render.php
+++ src/ofertas/render.php
@@ -17,7 +17,7 @@ $query = new WC_Product_Query(array(
 echo $products;
 ?>
 
-<p> <?php echo json_encode($products) ?> </p>
+<p> <?php echo json_encode($products[0]) ?> </p>
 
 <?php
 // TESTS -------------------------------------
