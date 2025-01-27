455303e361b7d4954ad853ae7709b67c60134777
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-26 21:59:58 -0300

    bloque de ofertas v1.1.1

--- build/ofertas/render.php
+++ build/ofertas/render.php
@@ -1,60 +1,11 @@
 
 
 <?php
-function get_product_with_highest_offer_title() {
-    // Obtén todos los productos en oferta
-    $query = new WC_Product_Query(array(
-        'limit' => -1, // Sin límite
-        'status' => 'publish', // Solo productos publicados
-        'meta_query' => array(
-            array(
-                'key' => '_sale_price', // Solo productos con precio de oferta
-                'value' => 0,
-                'compare' => '>',
-                'type' => 'NUMERIC'
-            )
-        )
-    ));
-    echo "<script>console.log('products: " . json_encode($query) . "');</script>";
-    $products = $query->get_products();
-    echo "<script>console.log('products: " . json_encode($products) . "');</script>";
-
-    // Variable para almacenar el producto con la mayor oferta
-    $highest_offer_product = null;
-    $highest_discount = 0;
-
-    // Itera sobre los productos para encontrar la mayor oferta
-    foreach ($products as $product) {
-        $regular_price = (float) $product->get_regular_price();
-        $sale_price = (float) $product->get_sale_price();
-        echo "<script>console.log('regular_price: " . json_encode($regular_price) . "');</script>";
-        echo "<script>console.log('sale_price: " . json_encode($sale_price) . "');</script>";
-
-        // Calcula el descuento
-        $discount = $regular_price - $sale_price;
-
-        // Actualiza el producto con la mayor oferta
-        if ($discount > $highest_discount) {
-            $highest_discount = $discount;
-            $highest_offer_product = $product;
-        }
-    }
-
-    // Retorna el título del producto con la mayor oferta
-    if ($highest_offer_product) {
-        return $highest_offer_product->get_name();
-    }
-
-    return 'No hay productos en oferta.';
-}
 
 ?>
 
 
-<p> <?php echo json_encode(get_product_with_highest_offer_title()) ?> </p>
-<?php
-echo "<script>console.log('Mensaje: " . json_encode(get_product_with_highest_offer_title()) . "');</script>";
-?>
+<p>[top_rated_products limit="4"]</p>
 <?php
 // TESTS -------------------------------------
 // Supongamos que $attributes contiene el JSON que mostraste
