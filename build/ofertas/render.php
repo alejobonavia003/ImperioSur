405a47cc29f131e467d31c9c097aac5f7c7deaa6
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-27 13:57:58 -0300

    bloque de ofertas v1.4.9

--- build/ofertas/render.php
+++ build/ofertas/render.php
@@ -64,7 +64,7 @@ $shortcode_content = isset($attributes['shortcode']) ? do_shortcode($attributes[
 
         foreach ($containers as $index => $container) {
             // Verificar si es el contenedor principal que deseas modificar
-            if ($container->parentNode->nodeName === 'div' && $container->parentNode->getAttribute('class') === 'product-block-content') {
+            if ($container->parentNode->nodeName === 'div' && $container->parentNode->getAttribute('class') == 'woocommerce') {
                 // Eliminar el atributo "class" del contenedor principal
                 if ($container->hasAttribute('class')) {
                     $container->removeAttribute('class');
