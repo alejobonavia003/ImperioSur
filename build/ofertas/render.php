5ccf8d62fe279b54fb998fb1003d168ea9a144e0
alejobonavia003 <bonaviaalejo@gmail.com>
2025-01-26 21:39:12 -0300

    bloque de ofertas v1.0.8

--- build/ofertas/render.php
+++ build/ofertas/render.php
@@ -1,18 +1,46 @@
 
 
 <?php
-$query = new WC_Product_Query(array(
-        'limit' => 4, // Sin límite
-        'status' => 'publish', // Solo productos publicados
-    ));
-    $products = $query->get_products();
-echo $products;
+  // Obtén todos los productos en oferta
+  $query = new WC_Product_Query(array(
+    'limit' => -1, // Sin límite
+    'status' => 'publish', // Solo productos publicados
+    'meta_query' => array(
+        array(
+            'key' => '_sale_price', // Solo productos con precio de oferta
+            'value' => 0,
+            'compare' => '>',
+            'type' => 'NUMERIC'
+        )
+    )
+));
+$products = $query->get_products();
+
+// Variable para almacenar el producto con la mayor oferta
+$highest_offer_product = null;
+$highest_discount = 0;
+
+// Itera sobre los productos para encontrar la mayor oferta
+foreach ($products as $product) {
+    $regular_price = (float) $product->get_regular_price();
+    $sale_price = (float) $product->get_sale_price();
+
+    // Calcula el descuento
+    $discount = $regular_price - $sale_price;
+
+    // Actualiza el producto con la mayor oferta
+    if ($discount > $highest_discount) {
+        $highest_discount = $discount;
+        $highest_offer_product = $product;
+    }
+}
+
 ?>
 
 
-<p> <?php echo json_encode($products[0]) ?> </p>
+<p> <?php echo json_encode($highest_offer_product) ?> </p>
 <?php
-echo "<script>console.log('Mensaje: " . json_encode($query) . "');</script>";
+echo "<script>console.log('Mensaje: " . json_encode($highest_offer_product) . "');</script>";
 ?>
 <?php
 // TESTS -------------------------------------
